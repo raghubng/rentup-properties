9eb400e896c81e9e271eefc934b3479e77b4bf2c
raghubng <114103601+raghubng@users.noreply.github.com>
2025-10-05 20:10:34 +0530

    Update Header.jsx
    header changes done

--- src/Component/common/header/Header.jsx
+++ src/Component/common/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <header>
         <div className='container flex'>
           <div className='logo'>
-            <img src='images/logo.png' alt=''/>
+        123
           </div>
           <div className='nav'>
             <ul className={navList ? "small" : "flex"}>
